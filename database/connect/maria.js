ca5de8d4e77b2df14f6e85cad5e06f374112bd65
necromman <necromman@gmail.com>
2023-03-14 14:07:45 +0900

    s

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -6,8 +6,7 @@ const conn = maria.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    insecureAuth: true,
-    charset: 'euckr'
+    insecureAuth: true
 });
 
 module.exports = conn;
