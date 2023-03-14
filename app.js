07a9e22a20323aeb18b5a25289fc9daaf5f642e2
necromman <necromman@gmail.com>
2023-03-14 15:49:41 +0900

    s

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(cors());
-app.use(bodyParser.json({defaultCharset: 'utf-8'}));
+app.use(bodyParser.json({defaultCharset: 'euckr'}));
 
 app.use('/api/',IndexRouter);
 
